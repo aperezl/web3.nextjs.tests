1500b946703ad87a9d19fad24ced2d8c02bdcd5b
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2022-08-18 21:18:10 +0200

    feat: add highlight support

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import '../styles/globals.css'
+import 'highlight.js/styles/atom-one-dark.css'
 
 function MyApp ({ Component, pageProps }) {
   return (
--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -37,6 +37,7 @@ export const getStaticProps = async ({ params }) => {
       rehypePlugins: [
         rehypeSlug,
         [rehypeAutolinkHeadings, { behavior: 'wrap' }],
+        rehypeHighlight,
         rehypeHighlight
       ]
     }
